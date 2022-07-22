178c1b4af3da8da0b4e281ff93ad7b8e6d18e8c6
badkem <hoangdinhthangloi@gmail.com>
2022-07-22 17:43:24 +0700

    commit

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
 document.body.style.border = "5px solid red";
+
+function handleResponse(message) {
+    console.log(`Message from the background script:  ${message.response}`);
+}
+
+function handleError(error) {
+    console.log(`Error: ${error}`);
+}
+
+function notifyBackgroundPage(e) {
+    let sending = browser.runtime.sendMessage({
+        greeting: "Greeting from the content script"
+    });
+    sending.then(handleResponse, handleError);
+}
+
+// window.addEventListener("click", notifyBackgroundPage);
+
+let audio = document.querySelector("audio");
+console.log(audio);
+
+audio.addEventListener("play", () => {
+    console.log("playing");
+    notifyBackgroundPage();
+});
+
+audio.addEventListener("pause", () => {
+    console.log("pause");
+});
--- scripts/background.js
+++ scripts/background.js
@@ -2,23 +2,42 @@ console.log("background script loaded");
 
 const url = "ws://localhost:8765";
 
-const socket = new WebSocket(url);
+function handleMessage(request, sender, sendResponse) {
+    console.log("Message from the content script: " +
+        request.greeting);
+    sendResponse({response: "Response from background script"});
+}
 
-socket.onopen = function () {
-    console.log("connected");
-    socket.send("Fuck you!");
-};
+browser.runtime.onMessage.addListener(handleMessage);
 
-socket.onmessage = function (msg) {
-    console.log("message:", msg.data);
-};
+// let audio = document.querySelector("audio");
+// console.log(audio);
+//
+// audio.addEventListener("play", () => {
+//     console.log("playing");
+// });
+//
+// audio.addEventListener("pause", () => {
+//     console.log("pause");
+// });
 
-socket.onerror = function (err) {
-    console.log("connect error:", err.message);
-};
-
-socket.onclose = function () {
-    console.log("disconnected");
-};
+// const socket = new WebSocket(url);
+//
+// socket.onopen = function () {
+//     console.log("connected");
+//     socket.send("Fuck you!");
+// };
+//
+// socket.onmessage = function (msg) {
+//     console.log("message:", msg.data);
+// };
+//
+// socket.onerror = function (err) {
+//     console.log("connect error:", err.message);
+// };
+//
+// socket.onclose = function () {
+//     console.log("disconnected");
+// };
 
 
