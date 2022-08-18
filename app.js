0da8fac259907b2fd8d2a2d0d553901285cf46ac
badkem <hoangdinhthangloi@gmail.com>
2022-08-18 08:25:16 +0700

    clean

--- app.js
+++ app.js
@@ -2,33 +2,17 @@ document.body.style.border = "5px solid red";
 const audio = document.querySelector("audio");
 const runTime = browser.runtime;
 
-function handleResponse(message) {
-    console.log(`Message from the background script:  ${message.response}`);
-}
-
-function handleError(error) {
-    console.log(`Error: ${error}`);
-}
-
 function notifyBackgroundPage(data) {
-    runTime
-        .sendMessage(data);
-        // .then(handleResponse, handleError);
+  runTime.sendMessage(data);
 }
 
 audio.addEventListener("play", () => {
-    console.log("playing");
-    const data = { playing: true };
-    const songTitle = document.getElementsByClassName("song-title-item");
+  const songTitle = document.getElementsByClassName("song-title-item");
+  const song_url = songTitle[0].querySelector("a").href.split("zingmp3.vn/")[1];
 
-    data.song_url = songTitle[0].querySelector("a").href.split("zingmp3.vn/")[1];
-
-    notifyBackgroundPage(data);
+  notifyBackgroundPage({ playing: true, song_url });
 });
 
 audio.addEventListener("pause", () => {
-    const data = {
-        playing: false,
-    };
-    notifyBackgroundPage(data);
+  notifyBackgroundPage({ playing: false });
 });
--- scripts/background.js
+++ scripts/background.js
@@ -1,80 +1,82 @@
 console.log("background script loaded");
-
-const baseUrl = "https://mp3.zing.vn/";
-
 const url = "ws://localhost:8765";
-
+const baseUrl = "https://mp3.zing.vn/";
 const runTime = browser.runtime;
 
 const socket = new WebSocket(url);
 
 getSongData = (key) => {
-    return new Promise((resolve) => {
-        const url = `${baseUrl}xhr/media/get-source?type=audio&key=${key}`;
-        const req = new XMLHttpRequest();
-        req.onload = () => {
-            if (req.status === 200) {
-                const data = JSON.parse(req.response);
-                const songData = data.data;
-                resolve(songData);
-            }
-            else resolve();
-        }
-        req.open("GET", url);
-        req.send();
-    });
-}
+  return new Promise((resolve) => {
+    const url = `${baseUrl}xhr/media/get-source?type=audio&key=${key}`;
+    const req = new XMLHttpRequest();
+    req.onload = () => {
+      if (req.status === 200) {
+        const data = JSON.parse(req.response);
+        const songData = data.data;
+        resolve(songData);
+      } else resolve();
+    };
+    req.open("GET", url);
+    req.send();
+  });
+};
 
 getSongKey = (data) => {
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(data, "text/html");
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(data, "text/html");
 
-    const secretValue = doc
-        .getElementsByClassName("player mt0")[0]
-        .attributes["data-xml"].value
+  const secretValue =
+    doc.getElementsByClassName("player mt0")[0].attributes["data-xml"].value;
 
-    return secretValue.split("key=")[1];
-}
+  return secretValue.split("key=")[1];
+};
 
 crawlSong = (songUrl) => {
+  return new Promise((resolve) => {
     const url = `${baseUrl}${songUrl}`;
     const req = new XMLHttpRequest();
     req.onload = () => {
-        if (req.status === 200) {
-            const key = getSongKey(req.response);
-            getSongData(key)
-                .then((res) => {
-                    console.log(res);
-                });
-        }
-    }
+      if (req.status === 200) {
+        resolve(getSongKey(req.response));
+      } else resolve();
+    };
     req.open("GET", url);
     req.send();
+  });
 };
 
 function handleMessage(res, sender, sendResponse) {
-    console.log("From content script:", res);
-    crawlSong(res.song_url);
-    // sendResponse({ response: "Response from background script" });
+  console.log("Incoming:", res);
+
+  const { playing, song_url } = res;
+
+  if (!playing) {
+    return socket.send(JSON.stringify(res));
+  }
+
+  crawlSong(song_url).then((key) => {
+    if (key.length > 0) res.key = key;
+
+    const songData = JSON.stringify(res);
+    socket.send(songData);
+  });
 }
 
 socket.onopen = function () {
-    console.log("connected");
-    socket.send("Fuck you!");
+  console.log("connected");
+  socket.send("Fuck you!");
 
-    runTime.onMessage.addListener(handleMessage);
+  runTime.onMessage.addListener(handleMessage);
 };
 
 socket.onmessage = function (msg) {
-    console.log("message:", msg.data);
+  console.log("message:", msg.data);
 };
 
 socket.onerror = function (err) {
-    console.log("connect error:", err.message);
+  console.log("connect error:", err.message);
 };
 
 socket.onclose = function () {
-    console.log("disconnected");
+  console.log("disconnected");
 };
-
-
